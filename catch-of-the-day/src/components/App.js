bbed4f183b645d319b81f9dde4d37d04ec49a902
Sarah Morris <sarah.morris9@gmail.com>
2020-03-14 11:47:43 -0500

    Added state, now updating state for added fish

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,22 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. take a copy of existing state
+    const fishes = { ...this.state.fishes };
+    // 2. add new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set new fishes object to state
+    this.setState({
+      fishes // this is the same as saying 'fishes: fishes', because our property and value names are the same
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +27,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
