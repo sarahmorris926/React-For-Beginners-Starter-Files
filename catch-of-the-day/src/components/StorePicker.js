824c1b52eaedbf5f5b59cb38b2994e5c28b386fa
Sarah Morris <sarah.morris9@gmail.com>
2020-03-13 22:53:30 -0500

    Added components

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
 class StorePicker extends React.Component {
-    render() {
-        return <p>I am the store picker!</p>
-    }
+  render() {
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store -></button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
